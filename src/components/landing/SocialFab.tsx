f55719d3d5626c64350fea90832aa3ae95381a6b
george <gtechong72@gmail.com>
2026-03-10 21:59:52 +0300

    Add vercel.json

--- src/components/landing/SocialFab.tsx
+++ src/components/landing/SocialFab.tsx
@@ -45,7 +45,7 @@ const SocialFab: React.FC = () => {
   ];
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-4">
+    <div className="fixed bottom-24 right-6 z-[999] flex flex-col items-end gap-4">
       {/* Expandable Social Menu */}
       <AnimatePresence>
         {isOpen && (
